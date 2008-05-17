3a3616bbbf8376c62bd7aeba286966ae057f5e52
Rael Dornfest <rael@Charlotte.local>
2008-05-16 17:12:56 -0700

    s/subscribable/subscription/

--- lib/freemium/acts_subscriber.rb
+++ lib/freemium/acts_subscriber.rb
@@ -9,21 +9,21 @@ module Freemium
 
       module ClassMethods
         def acts_as_subscriber(options = {})
-          subscription_model = options[:subscribable] || :subscription
+          subscription_model = options[:subscriber] || :subscription
           coupon_referrals_model = options[:coupon] || :coupon
           coupon_referrals_model = "#{class_name.underscore}_#{coupon_referrals_model}_referrals"
           
           
-          write_inheritable_attribute(:acts_as_subscribable_options, {
-            :subscribable_type => class_name.to_s,
+          write_inheritable_attribute(:acts_as_subscriber_options, {
+            :subscriber_type => class_name.to_s,
             :subcription_model_name => subscription_model,
             :coupon_referrals_model_name => coupon_referrals_model
           })          
-          class_inheritable_reader :acts_as_subscribable_options
+          class_inheritable_reader :acts_as_subscriber_options
           
           
-          has_one   subscription_model,      :dependent => :destroy, :foreign_key => :subscribable_id
-          has_many  coupon_referrals_model, :dependent => :destroy, :foreign_key => :subscribable_id
+          has_one   subscription_model,      :dependent => :destroy, :foreign_key => :subscriber_id
+          has_many  coupon_referrals_model, :dependent => :destroy, :foreign_key => :subscriber_id
           
           validates_uniqueness_of :referral_code, :case_sensitive => false, :allow_blank => true
           validates_format_of     :referral_code, :with => /\A#{Freemium.referral_code_prefix}/, :message => "must start with '#{Freemium.referral_code_prefix}'"
@@ -40,7 +40,7 @@ module Freemium
       module SingletonMethods
         
         def setup_referral_codes!
-          #do this in case the user has not added acts_as_subscribable yet....
+          #do this in case the user has not added acts_as_subscriber yet....
           send(:include, Freemium::Acts::Subscribable::InstanceMethods)
           find(:all, :select => 'id').each{|u| u.reset_referral_code!}
         end
@@ -70,7 +70,7 @@ module Freemium
         end
         
         def apply_coupon_referral_code?(code)
-          subscription_ = self.send("#{acts_as_subscribable_options[:subcription_model_name]}")
+          subscription_ = self.send("#{acts_as_subscriber_options[:subcription_model_name]}")
           
           return false if subscription.blank?
           @comp_code = code
@@ -89,7 +89,7 @@ module Freemium
           #   #we do
           #   if Freemium.referral_allowed_after_signup #and do some sort of signup check..
           #     #lets make sure they haven't used it already....
-          #     if eval("self.#{acts_as_subscribable_options[:coupon_referrals_model_name]}.count(:conditions => {:subscribable_id => u.id})")
+          #     if eval("self.#{acts_as_subscriber_options[:coupon_referrals_model_name]}.count(:conditions => {:subscriber_id => u.id})")
           #       errors.add_to_base("You have already used this referral code.") 
           #       return false;
           #     end
@@ -102,17 +102,17 @@ module Freemium
           #   #we need to apply free days to the user who is using the code AND the user it is coming from.
           #   
           #   #apply to the subscription o the current user
-          #   eval("self.#{acts_as_subscribable_options[:coupon_referrals_model_name]}.build(:referring_user_id => u.id, :#{acts_as_subscribable_options[:subcription_model_name]} => subscription, :free_days => Freemium.referral_days_for_applied_user)")
+          #   eval("self.#{acts_as_subscriber_options[:coupon_referrals_model_name]}.build(:referring_user_id => u.id, :#{acts_as_subscriber_options[:subcription_model_name]} => subscription, :free_days => Freemium.referral_days_for_applied_user)")
           #   
           #   #apply to the subscription of the referring user
           #   unless u.subscription.blank?
           #     #should never have a blank subscription, but just in 
-          #     @referring_users_comp = eval("u.#{acts_as_subscribable_options[:coupon_referrals_model_name]}.build(:referring_user_id => u.id, :#{acts_as_subscribable_options[:subcription_model_name]} => u.subscription, :free_days => Freemium.referral_days_for_referred_user)")
+          #     @referring_users_comp = eval("u.#{acts_as_subscriber_options[:coupon_referrals_model_name]}.build(:referring_user_id => u.id, :#{acts_as_subscriber_options[:subcription_model_name]} => u.subscription, :free_days => Freemium.referral_days_for_referred_user)")
           #   end
           # else
           #   c = Coupon.find_by_coupon_code(code)
           #   return false if c.blank?
-          #   eval("self.#{acts_as_subscribable_options[:coupon_referrals_model_name]}.build(:coupon_id => c.id, :#{acts_as_subscribable_options[:subcription_model_name]} => subscription, :free_days => c.span_num_days)")
+          #   eval("self.#{acts_as_subscriber_options[:coupon_referrals_model_name]}.build(:coupon_id => c.id, :#{acts_as_subscriber_options[:subcription_model_name]} => subscription, :free_days => c.span_num_days)")
           # end
         end
         
@@ -138,7 +138,7 @@ puts "about prep_comps-1"
             #we do
             if Freemium.referral_allowed_after_signup 
               #lets make sure they haven't used it already....
-              if eval("self.#{acts_as_subscribable_options[:coupon_referrals_model_name]}.count(:conditions => {:referring_user_id => u.id, :subscribable_id => self.id})")
+              if eval("self.#{acts_as_subscriber_options[:coupon_referrals_model_name]}.count(:conditions => {:referring_user_id => u.id, :subscriber_id => self.id})")
                 errors.add_to_base("You have already used this referral code.") 
                 return false;
               end
@@ -152,13 +152,13 @@ puts "about prep_comps-2"
             #we need to apply free days to the user who is using the code AND the user it is coming from.
             
             #apply to the subscription o the current user
-            eval("self.#{acts_as_subscribable_options[:coupon_referrals_model_name]}.build(:referring_user_id => u.id, :#{acts_as_subscribable_options[:subcription_model_name]} => subscription, :free_days => Freemium.referral_days_for_applied_user)")
+            eval("self.#{acts_as_subscriber_options[:coupon_referrals_model_name]}.build(:referring_user_id => u.id, :#{acts_as_subscriber_options[:subcription_model_name]} => subscription, :free_days => Freemium.referral_days_for_applied_user)")
 puts "about prep_comps-3"
             
             #apply to the subscription of the referring user
             unless u.subscription.blank?
               #should never have a blank subscription, but just in 
-              @referring_users_comp = eval("u.#{acts_as_subscribable_options[:coupon_referrals_model_name]}.build(:referring_user_id => u.id, :#{acts_as_subscribable_options[:subcription_model_name]} => u.subscription, :free_days => Freemium.referral_days_for_referred_user)")
+              @referring_users_comp = eval("u.#{acts_as_subscriber_options[:coupon_referrals_model_name]}.build(:referring_user_id => u.id, :#{acts_as_subscriber_options[:subcription_model_name]} => u.subscription, :free_days => Freemium.referral_days_for_referred_user)")
 puts "about prep_comps-4"
             end
           else
@@ -169,12 +169,12 @@ puts "about prep_comps-4"
             end
             
             #make sure it hasn't been applied before
-            if eval("self.#{acts_as_subscribable_options[:coupon_referrals_model_name]}.count(:conditions => {:coupon_id => c.id, :subscribable_id => self.id})")
+            if eval("self.#{acts_as_subscriber_options[:coupon_referrals_model_name]}.count(:conditions => {:coupon_id => c.id, :subscriber_id => self.id})")
               errors.add_to_base("You have already used this coupon code.") 
               return false;
             end
             
-            eval("self.#{acts_as_subscribable_options[:coupon_referrals_model_name]}.build(:coupon_id => c.id, :#{acts_as_subscribable_options[:subcription_model_name]} => subscription, :free_days => c.span_num_days)")
+            eval("self.#{acts_as_subscriber_options[:coupon_referrals_model_name]}.build(:coupon_id => c.id, :#{acts_as_subscriber_options[:subcription_model_name]} => subscription, :free_days => c.span_num_days)")
           end
         end
         
